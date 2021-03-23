28d38b49d3d57f636b25c4464eeddf5746c3fc1f
rbaudoin <77682851+rbaudoin@users.noreply.github.com>
2021-03-23 10:40:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <?php
 require 'vendor/autoload.php';
- 
+
+//issuer se encuentra en  /.well-known/openid-configuration, client id, client secret se encuentran en el client oAuth2 del realm 
 $issuer = 'http://am.demo.com/openam/oauth2/realms/root/realms/Rocio';
 $cid = 'user';
 $secret = 'iden2006';
-
 $oidc = new Jumbojett\OpenIDConnectClient($issuer, $cid, $secret);
- 
-$oidc->authenticate();
- $oidc->addScope('openid','profile', 'email');
 
+//agregar los scopes necesarios  
+$oidc->addScope(array('openid', 'email', 'profile'));
+
+$oidc->authenticate();
+
+
+//agregar los valores que se quieren  mostrar en pantalla 
 $requestUserInfo= $oidc->requestUserInfo(null);
-$name= $oidc->requestUserInfo('sub');
+$given_name= $oidc->requestUserInfo('given_name');
+$family_name=$oidc->requestUserInfo('family_name');
+$name= $oidc->requestUserInfo('name');
 $email = $oidc->requestUserInfo('email');
+$sub= $oidc->requestUserInfo('sub');
 $accesstoken=$oidc->getAccessToken();
 $refreshtoken=$oidc->getRefreshToken();
 $idtoken=$oidc->getIdToken();
 
+//bucle foreach para recorrer todos los elementos de requiestUserInfo
 $session = array();
     foreach($oidc->requestUserInfo() as $key=> $value){
         if(is_array($value)){
@@ -26,14 +34,25 @@ $session = array();
         }
         $session[$key] = $v;
     }
-
+//imprimir en pantalla los valores 
+echo "#given_name:".PHP_EOL;
+echo $given_name;
+echo "<br>";
+echo "<br>";
+echo "#family_name:".PHP_EOL;
+echo $family_name;
+echo "<br>";
+echo "<br>";
 echo "#name:".PHP_EOL;
 echo $name;
 echo "<br>";
 echo "<br>";
 echo "#email:".PHP_EOL;
-echo "<br>";
 echo $email;
+echo "<br>";
+echo "<br>";
+echo "#sub:".PHP_EOL;
+echo $sub;
 echo "<br>";
 echo "<br>";
 echo "#refreshToken:".PHP_EOL;
@@ -53,24 +72,22 @@ echo "<br>";
 echo "<br>";
 echo "#requestUserInfo(null):".PHP_EOL;
 echo "<br>";
+//serialize para convertir objetos en string 
 echo serialize($requestUserInfo);
 echo "<br>";
 echo "<br>";
 echo "#UserInfo (foreach):".PHP_EOL;
 echo "<br>";
+//implode para convertir array en string 
 echo implode($session);
 echo "<br>";
 echo "<br>";
 
-
-
-
+/* boton de redireccionamiento a logout */
 ?>
 
 <a href="logout.php">
 click here to log out</a>
-
-
 
 
 
